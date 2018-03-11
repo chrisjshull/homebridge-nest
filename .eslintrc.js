4fedcec8680ec799a045522ddf620027e51fff4e
Chris J. Shull <chrisjshull@gmail.com>
2018-03-11 11:27:18 -0700

    add eslint and editorconfig #9

---
+++ .eslintrc.js
@@ -0,0 +1,296 @@
+module.exports = {
+    "env": {
+        "es6": true,
+        "node": true
+    },
+    "extends": "eslint:recommended",
+    "rules": {
+        "accessor-pairs": "error",
+        "array-bracket-newline": "error",
+        "array-bracket-spacing": [
+            "error",
+            "never"
+        ],
+        "array-callback-return": "off",
+        "array-element-newline": "off",
+        "arrow-body-style": "error",
+        "arrow-parens": "error",
+        "arrow-spacing": "error",
+        "block-scoped-var": "error",
+        "block-spacing": [
+            "error",
+            "always"
+        ],
+        "brace-style": [
+            "error",
+            "1tbs",
+            {
+                "allowSingleLine": true
+            }
+        ],
+        "callback-return": "off",
+        "camelcase": [
+            "error",
+            {
+                "properties": "never"
+            }
+        ],
+        "capitalized-comments": "off",
+        "class-methods-use-this": "error",
+        "comma-dangle": "error",
+        "comma-spacing": [
+            "error",
+            {
+                "after": true,
+                "before": false
+            }
+        ],
+        "comma-style": [
+            "error",
+            "last"
+        ],
+        "complexity": "error",
+        "computed-property-spacing": [
+            "error",
+            "never"
+        ],
+        "consistent-return": "off",
+        "consistent-this": "off",
+        "curly": "off",
+        "default-case": "off",
+        "dot-location": [
+            "error",
+            "property"
+        ],
+        "dot-notation": [
+            "error",
+            {
+                "allowKeywords": true
+            }
+        ],
+        "eol-last": "error",
+        "eqeqeq": "off",
+        "for-direction": "error",
+        "func-call-spacing": "error",
+        "func-name-matching": "error",
+        "func-names": [
+            "error",
+            "never"
+        ],
+        "func-style": "off",
+        "function-paren-newline": "off",
+        "generator-star-spacing": "error",
+        "getter-return": "error",
+        "global-require": "error",
+        "guard-for-in": "off",
+        "handle-callback-err": "error",
+        "id-blacklist": "error",
+        "id-length": "off",
+        "id-match": "error",
+        "implicit-arrow-linebreak": "error",
+        "indent": ["error", 2],
+        "indent-legacy": "off",
+        "init-declarations": "off",
+        "jsx-quotes": "error",
+        "key-spacing": "error",
+        "keyword-spacing": [
+            "error",
+            {
+                "after": true,
+                "before": true
+            }
+        ],
+        //"line-comment-position": "error",
+        "linebreak-style": [
+            "error",
+            "unix"
+        ],
+        "lines-around-comment": "error",
+        "lines-around-directive": "error",
+        "lines-between-class-members": "error",
+        "max-depth": "error",
+        "max-len": "off",
+        // "max-lines": "error",
+        "max-nested-callbacks": "error",
+        "max-params": "off",
+        "max-statements": "off",
+        "max-statements-per-line": "error",
+        "multiline-comment-style": "error",
+        "new-cap": "error",
+        "new-parens": "error",
+        "newline-after-var": "off",
+        "newline-before-return": "off",
+        "newline-per-chained-call": "error",
+        "no-alert": "error",
+        "no-array-constructor": "error",
+        "no-await-in-loop": "error",
+        "no-bitwise": "off",
+        "no-buffer-constructor": "error",
+        "no-caller": "error",
+        "no-catch-shadow": "error",
+        "no-confusing-arrow": "error",
+        // "no-continue": "error",
+        "no-div-regex": "error",
+        "no-duplicate-imports": "error",
+        "no-else-return": "off",
+        "no-empty-function": "error",
+        "no-eq-null": "error",
+        "no-eval": "error",
+        "no-extend-native": "error",
+        "no-extra-bind": "error",
+        "no-extra-label": "error",
+        "no-extra-parens": "off",
+        "no-floating-decimal": "error",
+        "no-implicit-coercion": "error",
+        "no-implicit-globals": "error",
+        "no-implied-eval": "error",
+        //"no-inline-comments": "error",
+        "no-inner-declarations": [
+            "error",
+            "functions"
+        ],
+        "no-invalid-this": "error",
+        "no-iterator": "error",
+        "no-label-var": "error",
+        "no-labels": "error",
+        "no-lone-blocks": "error",
+        "no-lonely-if": "error",
+        "no-loop-func": "error",
+        "no-magic-numbers": "off",
+        "no-mixed-operators": "error",
+        "no-mixed-requires": "error",
+        "no-multi-assign": "error",
+        "no-multi-spaces": "error",
+        "no-multi-str": "error",
+        "no-multiple-empty-lines": "error",
+        "no-native-reassign": "error",
+        "no-negated-condition": "off",
+        "no-negated-in-lhs": "error",
+        "no-nested-ternary": "error",
+        "no-new": "error",
+        "no-new-func": "error",
+        "no-new-object": "error",
+        "no-new-require": "error",
+        "no-new-wrappers": "error",
+        "no-octal-escape": "error",
+        "no-param-reassign": "off",
+        "no-path-concat": "error",
+        "no-plusplus": "error",
+        "no-process-env": "error",
+        "no-process-exit": "error",
+        "no-proto": "error",
+        "no-prototype-builtins": "error",
+        "no-restricted-globals": "error",
+        "no-restricted-imports": "error",
+        "no-restricted-modules": "error",
+        "no-restricted-properties": "error",
+        "no-restricted-syntax": "error",
+        "no-return-assign": [
+            "error",
+            "except-parens"
+        ],
+        "no-return-await": "error",
+        "no-script-url": "error",
+        "no-self-compare": "error",
+        "no-sequences": "error",
+        "no-shadow": "error",
+        "no-shadow-restricted-names": "error",
+        "no-spaced-func": "error",
+        "no-sync": "error",
+        "no-tabs": "off",
+        "no-template-curly-in-string": "error",
+        "no-ternary": "off",
+        "no-throw-literal": "error",
+        "no-trailing-spaces": "error",
+        "no-undef-init": "error",
+        // "no-undefined": "error",
+        "no-underscore-dangle": "error",
+        "no-unmodified-loop-condition": "error",
+        "no-unneeded-ternary": "off",
+        "no-unused-expressions": "off",
+        "no-unused-vars": ["error", { "vars": "all", "args": "none", "ignoreRestSiblings": true }],
+        "no-use-before-define": "off",
+        "no-useless-call": "error",
+        "no-useless-computed-key": "error",
+        //"no-useless-concat": "error",
+        "no-useless-constructor": "error",
+        "no-useless-rename": "error",
+        "no-useless-return": "error",
+        "no-var": "error",
+        //"no-void": "error",
+        "no-warning-comments": "warn",
+        "no-whitespace-before-property": "error",
+        "no-with": "error",
+        "nonblock-statement-body-position": "error",
+        "object-curly-newline": "off",
+        "object-curly-spacing": "error",
+        "object-property-newline": "error",
+        "object-shorthand": "off",
+        "one-var": "off",
+        "one-var-declaration-per-line": [
+            "error",
+            "initializations"
+        ],
+        "operator-assignment": [
+            "error",
+            "always"
+        ],
+        "operator-linebreak": "error",
+        "padded-blocks": "off",
+        "padding-line-between-statements": "error",
+        "prefer-arrow-callback": "off",
+        "prefer-const": ["error", {"destructuring": "all"}],
+        "prefer-destructuring": "off",
+        "prefer-numeric-literals": "error",
+        "prefer-promise-reject-errors": "error",
+        "prefer-reflect": "off",
+        //"prefer-rest-params": "error",
+        "prefer-spread": "error",
+        "prefer-template": "off",
+        "quote-props": "off",
+        "quotes": "off",
+        "radix": "error",
+        "require-await": "error",
+        "require-jsdoc": "off",
+        "rest-spread-spacing": "error",
+        "semi": "error",
+        "semi-spacing": "error",
+        "semi-style": [
+            "error",
+            "last"
+        ],
+        "sort-imports": "error",
+        "sort-keys": "off",
+        "sort-vars": "off",
+        "space-before-blocks": "off",
+        "space-before-function-paren": "off",
+        "space-in-parens": "off",
+        "space-infix-ops": "error",
+        "space-unary-ops": "error",
+        "spaced-comment": [
+            "error",
+            "always"
+        ],
+        "strict": [
+            "error",
+            "never"
+        ],
+        "switch-colon-spacing": "error",
+        "symbol-description": "error",
+        "template-curly-spacing": "error",
+        "template-tag-spacing": "error",
+        "unicode-bom": [
+            "error",
+            "never"
+        ],
+        "valid-jsdoc": "error",
+        "vars-on-top": "off",
+        "wrap-iife": "error",
+        "wrap-regex": "error",
+        "yield-star-spacing": "error",
+        "yoda": [
+            "error",
+            "never"
+        ]
+    }
+};
--- index.js
+++ index.js
@@ -1,276 +1,274 @@
-var NestConnection = require('./lib/nest-connection.js');
-var inherits = require('util').inherits;
-var Promise = require('bluebird');
+const NestConnection = require('./lib/nest-connection.js');
+const inherits = require('util').inherits;
+const Promise = require('bluebird');
 
-var Service, Characteristic, Accessory, uuid;
-var DeviceAccessory, ThermostatAccessory, ProtectAccessory, CamAccessory;
-var Away, EcoMode, FanTimerActive, FanTimerDuration, HasLeaf, ManualTestActive, SunlightCorrectionEnabled, SunlightCorrectionActive, UsingEmergencyHeat;
+let Service, Characteristic, Accessory, uuid;
+let ThermostatAccessory, ProtectAccessory, CamAccessory; // DeviceAccessory,
+let Away, EcoMode, FanTimerActive, FanTimerDuration, HasLeaf, ManualTestActive, SunlightCorrectionEnabled, SunlightCorrectionActive, UsingEmergencyHeat;
 
 module.exports = function (homebridge) {
-	Service = homebridge.hap.Service;
-	Characteristic = homebridge.hap.Characteristic;
-	Accessory = homebridge.hap.Accessory;
-	uuid = homebridge.hap.uuid;
+  Service = homebridge.hap.Service;
+  Characteristic = homebridge.hap.Characteristic;
+  Accessory = homebridge.hap.Accessory;
+  uuid = homebridge.hap.uuid;
 
-// Define custom characteristics
+  // Define custom characteristics
 
-	/**
-	 * Characteristic "Away"
-	 */
-	Away = function () {
-		Characteristic.call(this, 'Away', 'D6D47D29-4638-4F44-B53C-D84015DAEBDB');
-		this.setProps({
-			format: Characteristic.Formats.BOOL,
-			perms: [Characteristic.Perms.READ, Characteristic.Perms.WRITE, Characteristic.Perms.NOTIFY]
-		});
-		this.value = this.getDefaultValue();
-	};
-	inherits(Away, Characteristic);
+  /*
+   * Characteristic "Away"
+   */
+  Away = function () {
+    Characteristic.call(this, 'Away', 'D6D47D29-4638-4F44-B53C-D84015DAEBDB');
+    this.setProps({
+      format: Characteristic.Formats.BOOL,
+      perms: [Characteristic.Perms.READ, Characteristic.Perms.WRITE, Characteristic.Perms.NOTIFY]
+    });
+    this.value = this.getDefaultValue();
+  };
+  inherits(Away, Characteristic);
 
-	/**
-	 * Characteristic "EcoMode"
-	 */
-	EcoMode = function () {
-		Characteristic.call(this, 'Eco Mode', 'D6D47D29-4639-4F44-B53C-D84015DAEBDB');
-		this.setProps({
-			format: Characteristic.Formats.BOOL,
-			perms: [Characteristic.Perms.READ, Characteristic.Perms.WRITE, Characteristic.Perms.NOTIFY]
-		});
-		this.value = this.getDefaultValue();
-	};
-	inherits(EcoMode, Characteristic);
+  /*
+   * Characteristic "EcoMode"
+   */
+  EcoMode = function () {
+    Characteristic.call(this, 'Eco Mode', 'D6D47D29-4639-4F44-B53C-D84015DAEBDB');
+    this.setProps({
+      format: Characteristic.Formats.BOOL,
+      perms: [Characteristic.Perms.READ, Characteristic.Perms.WRITE, Characteristic.Perms.NOTIFY]
+    });
+    this.value = this.getDefaultValue();
+  };
+  inherits(EcoMode, Characteristic);
 
-	/**
-	 * Characteristic "FanTimerActive"
-	 */
-	FanTimerActive = function () {
-		Characteristic.call(this, 'Fan Timer Active', 'D6D47D29-4640-4F44-B53C-D84015DAEBDB');
-		this.setProps({
-			format: Characteristic.Formats.BOOL,
-			perms: [Characteristic.Perms.READ, Characteristic.Perms.WRITE, Characteristic.Perms.NOTIFY]
-		});
-		this.value = this.getDefaultValue();
-	};
-	inherits(FanTimerActive, Characteristic);
+  /*
+   * Characteristic "FanTimerActive"
+   */
+  FanTimerActive = function () {
+    Characteristic.call(this, 'Fan Timer Active', 'D6D47D29-4640-4F44-B53C-D84015DAEBDB');
+    this.setProps({
+      format: Characteristic.Formats.BOOL,
+      perms: [Characteristic.Perms.READ, Characteristic.Perms.WRITE, Characteristic.Perms.NOTIFY]
+    });
+    this.value = this.getDefaultValue();
+  };
+  inherits(FanTimerActive, Characteristic);
 
-	/**
-	 * Characteristic "FanTimerDuration"
-	 */
-	FanTimerDuration = function () {
-		Characteristic.call(this, 'Fan Timer Duraton', 'D6D47D29-4641-4F44-B53C-D84015DAEBDB');
-		this.setProps({
-			format: Characteristic.Formats.UINT8,
-			unit: Characteristic.Units.MINUTES,
-			maxValue: 60,
-			minValue: 15,
-			minStep: 15,
-			perms: [Characteristic.Perms.READ, Characteristic.Perms.WRITE, Characteristic.Perms.NOTIFY]
-		});
-		this.value = this.getDefaultValue();
-	};
-	inherits(FanTimerDuration, Characteristic);
+  /*
+   * Characteristic "FanTimerDuration"
+   */
+  FanTimerDuration = function () {
+    Characteristic.call(this, 'Fan Timer Duraton', 'D6D47D29-4641-4F44-B53C-D84015DAEBDB');
+    this.setProps({
+      format: Characteristic.Formats.UINT8,
+      unit: Characteristic.Units.MINUTES,
+      maxValue: 60,
+      minValue: 15,
+      minStep: 15,
+      perms: [Characteristic.Perms.READ, Characteristic.Perms.WRITE, Characteristic.Perms.NOTIFY]
+    });
+    this.value = this.getDefaultValue();
+  };
+  inherits(FanTimerDuration, Characteristic);
 
-	/**
-	 * Characteristic "HasLeaf"
-	 */
-	HasLeaf = function () {
-		Characteristic.call(this, 'Has Leaf', 'D6D47D29-4642-4F44-B53C-D84015DAEBDB');
-		this.setProps({
-			format: Characteristic.Formats.BOOL,
-			perms: [Characteristic.Perms.READ, Characteristic.Perms.NOTIFY]
-		});
-		this.value = this.getDefaultValue();
-	};
-	inherits(HasLeaf, Characteristic);
+  /*
+   * Characteristic "HasLeaf"
+   */
+  HasLeaf = function () {
+    Characteristic.call(this, 'Has Leaf', 'D6D47D29-4642-4F44-B53C-D84015DAEBDB');
+    this.setProps({
+      format: Characteristic.Formats.BOOL,
+      perms: [Characteristic.Perms.READ, Characteristic.Perms.NOTIFY]
+    });
+    this.value = this.getDefaultValue();
+  };
+  inherits(HasLeaf, Characteristic);
 
-	/**
-	 * Characteristic "ManualTestActive"
-	 */
-	ManualTestActive = function () {
-		Characteristic.call(this, 'Manual Test Active', 'D6D47D29-4643-4F44-B53C-D84015DAEBDB');
-		this.setProps({
-			format: Characteristic.Formats.BOOL,
-			perms: [Characteristic.Perms.READ, Characteristic.Perms.NOTIFY]
-		});
-		this.value = this.getDefaultValue();
-	};
-	inherits(ManualTestActive, Characteristic);
+  /*
+   * Characteristic "ManualTestActive"
+   */
+  ManualTestActive = function () {
+    Characteristic.call(this, 'Manual Test Active', 'D6D47D29-4643-4F44-B53C-D84015DAEBDB');
+    this.setProps({
+      format: Characteristic.Formats.BOOL,
+      perms: [Characteristic.Perms.READ, Characteristic.Perms.NOTIFY]
+    });
+    this.value = this.getDefaultValue();
+  };
+  inherits(ManualTestActive, Characteristic);
 
-	/**
-	 * Characteristic "SunlightCorrectionEnabled"
-	 */
-	SunlightCorrectionEnabled = function () {
-		Characteristic.call(this, 'Sunlight Correction Enabled', 'D6D47D29-4644-4F44-B53C-D84015DAEBDB');
-		this.setProps({
-			format: Characteristic.Formats.BOOL,
-			perms: [Characteristic.Perms.READ, Characteristic.Perms.NOTIFY]
-		});
-		this.value = this.getDefaultValue();
-	};
-	inherits(SunlightCorrectionEnabled, Characteristic);
+  /*
+   * Characteristic "SunlightCorrectionEnabled"
+   */
+  SunlightCorrectionEnabled = function () {
+    Characteristic.call(this, 'Sunlight Correction Enabled', 'D6D47D29-4644-4F44-B53C-D84015DAEBDB');
+    this.setProps({
+      format: Characteristic.Formats.BOOL,
+      perms: [Characteristic.Perms.READ, Characteristic.Perms.NOTIFY]
+    });
+    this.value = this.getDefaultValue();
+  };
+  inherits(SunlightCorrectionEnabled, Characteristic);
 
-	/**
-	 * Characteristic "SunlightCorrectionActive"
-	 */
-	SunlightCorrectionActive = function () {
-		Characteristic.call(this, 'Sunlight Correction Active', 'D6D47D29-4645-4F44-B53C-D84015DAEBDB');
-		this.setProps({
-			format: Characteristic.Formats.BOOL,
-			perms: [Characteristic.Perms.READ, Characteristic.Perms.NOTIFY]
-		});
-		this.value = this.getDefaultValue();
-	};
-	inherits(SunlightCorrectionActive, Characteristic);
+  /*
+   * Characteristic "SunlightCorrectionActive"
+   */
+  SunlightCorrectionActive = function () {
+    Characteristic.call(this, 'Sunlight Correction Active', 'D6D47D29-4645-4F44-B53C-D84015DAEBDB');
+    this.setProps({
+      format: Characteristic.Formats.BOOL,
+      perms: [Characteristic.Perms.READ, Characteristic.Perms.NOTIFY]
+    });
+    this.value = this.getDefaultValue();
+  };
+  inherits(SunlightCorrectionActive, Characteristic);
 
-	/**
-	 * Characteristic "UsingEmergencyHeat"
-	 */
-	UsingEmergencyHeat = function () {
-		Characteristic.call(this, 'Using Emergency Heat', 'D6D47D29-4646-4F44-B53C-D84015DAEBDB');
-		this.setProps({
-			format: Characteristic.Formats.BOOL,
-			perms: [Characteristic.Perms.READ, Characteristic.Perms.NOTIFY]
-		});
-		this.value = this.getDefaultValue();
-	};
-	inherits(UsingEmergencyHeat, Characteristic);
+  /*
+   * Characteristic "UsingEmergencyHeat"
+   */
+  UsingEmergencyHeat = function () {
+    Characteristic.call(this, 'Using Emergency Heat', 'D6D47D29-4646-4F44-B53C-D84015DAEBDB');
+    this.setProps({
+      format: Characteristic.Formats.BOOL,
+      perms: [Characteristic.Perms.READ, Characteristic.Perms.NOTIFY]
+    });
+    this.value = this.getDefaultValue();
+  };
+  inherits(UsingEmergencyHeat, Characteristic);
 
-	var exportedTypes = {
-		Accessory: Accessory,
-		Service: Service,
-		Characteristic: Characteristic,
-		uuid: uuid,
-		Away: Away,
-		EcoMode: EcoMode,
-		FanTimerActive: FanTimerActive,
-		FanTimerDuration: FanTimerDuration,
-		HasLeaf: HasLeaf,
-		ManualTestActive: ManualTestActive,
-		SunlightCorrectionEnabled: SunlightCorrectionEnabled,
-		SunlightCorrectionActive: SunlightCorrectionActive,
-		UsingEmergencyHeat: UsingEmergencyHeat
-	};
+  const exportedTypes = {
+    Accessory: Accessory,
+    Service: Service,
+    Characteristic: Characteristic,
+    uuid: uuid,
+    Away: Away,
+    EcoMode: EcoMode,
+    FanTimerActive: FanTimerActive,
+    FanTimerDuration: FanTimerDuration,
+    HasLeaf: HasLeaf,
+    ManualTestActive: ManualTestActive,
+    SunlightCorrectionEnabled: SunlightCorrectionEnabled,
+    SunlightCorrectionActive: SunlightCorrectionActive,
+    UsingEmergencyHeat: UsingEmergencyHeat
+  };
 
-	DeviceAccessory = require('./lib/nest-device-accessory.js')(exportedTypes);
-	ThermostatAccessory = require('./lib/nest-thermostat-accessory.js')(exportedTypes);
-	ProtectAccessory = require('./lib/nest-protect-accessory.js')(exportedTypes);
-	CamAccessory = require('./lib/nest-cam-accessory.js')(exportedTypes);
+  // DeviceAccessory = require('./lib/nest-device-accessory.js')(exportedTypes);
+  ThermostatAccessory = require('./lib/nest-thermostat-accessory.js')(exportedTypes); // eslint-disable-line global-require
+  ProtectAccessory = require('./lib/nest-protect-accessory.js')(exportedTypes); // eslint-disable-line global-require
+  CamAccessory = require('./lib/nest-cam-accessory.js')(exportedTypes); // eslint-disable-line global-require
 
-	homebridge.registerPlatform("homebridge-nest", "Nest", NestPlatform);
+  homebridge.registerPlatform("homebridge-nest", "Nest", NestPlatform);
 };
 
 function NestPlatform(log, config) {
-	// auth info
-	this.config = config;
+  // auth info
+  this.config = config;
 
-	this.log = log;
-	this.accessoryLookup = {};
+  this.log = log;
+  this.accessoryLookup = {};
 }
 
-var setupConnection = function(config, log) {
-	return new Promise(function (resolve, reject) {
-		var token = config["token"];
-		var clientId = config["clientId"];
-		var clientSecret = config["clientSecret"];
-		var code = config["code"];
-		var authURL = clientId ? "https://home.nest.com/login/oauth2?client_id=" + clientId + "&state=STATE" : null;
+const setupConnection = function(config, log) {
+  return new Promise(function (resolve, reject) {
+    const token = config.token;
+    const clientId = config.clientId;
+    const clientSecret = config.clientSecret;
+    const code = config.code;
+    const authURL = clientId ? "https://home.nest.com/login/oauth2?client_id=" + clientId + "&state=STATE" : null;
 
-		var err;
-		if (!token && !clientId && !clientSecret && !code) {
-			err = "You did not specify {'token'} or {'clientId','clientSecret','code'}, one set of which is required for the new API";
-		} else if (!token && clientId && clientSecret && !code) {
-			err = "You are missing the one-time-use 'code' param. Should be able to obtain from " + authURL;
-		} else if (!token && (!clientId || !clientSecret || !code)) {
-			err = "If you are going to use {'clientId','clientSecret','code'} then you must specify all three, otherwise use {'token'}";
-		}
-		if (err) {
-			reject(new Error(err));
-			return;
-		}
+    let err;
+    if (!token && !clientId && !clientSecret && !code) {
+      err = "You did not specify {'token'} or {'clientId','clientSecret','code'}, one set of which is required for the new API";
+    } else if (!token && clientId && clientSecret && !code) {
+      err = "You are missing the one-time-use 'code' param. Should be able to obtain from " + authURL;
+    } else if (!token && (!clientId || !clientSecret || !code)) {
+      err = "If you are going to use {'clientId','clientSecret','code'} then you must specify all three, otherwise use {'token'}";
+    }
+    if (err) {
+      reject(new Error(err));
+      return;
+    }
 
-		var conn = new NestConnection(token, log);
-		if (token) {
-			resolve(conn);
-		} else {
-			conn.auth(clientId, clientSecret, code)
-				.then(function(token) {
-					if (log) log.warn("CODE IS ONLY VALID ONCE! Update config to use {'token':'" + token + "'} instead.");
-					resolve(conn);
-				})
-				.catch(function(err){
-					reject(err);
-					if (log) log.warn("Auth failed which likely means the code is no longer valid. Should be able to generate a new one at " + authURL);
-				});
-		}
-	});
+    const conn = new NestConnection(token, log);
+    if (token) {
+      resolve(conn);
+    } else {
+      conn.auth(clientId, clientSecret, code)
+        .then(function(newToken) {
+          if (log) log.warn("CODE IS ONLY VALID ONCE! Update config to use {'token':'" + newToken + "'} instead.");
+          resolve(conn);
+        })
+        .catch(function(authError){
+          reject(authError);
+          if (log) log.warn("Auth failed which likely means the code is no longer valid. Should be able to generate a new one at " + authURL);
+        });
+    }
+  });
 };
 
 NestPlatform.prototype = {
-	accessories: function (callback) {
-		this.log("Fetching Nest devices.");
+  accessories: function (callback) {
+    this.log("Fetching Nest devices.");
 
-		var that = this;
+    const that = this;
 
-		var generateAccessories = function(data) {
-			var foundAccessories = [];
+    const generateAccessories = function(data) {
+      const foundAccessories = [];
 
-			var loadDevices = function(DeviceType) {
-				var list = data.devices && data.devices[DeviceType.deviceGroup];
-				for (var deviceId in list) {
-					if (list.hasOwnProperty(deviceId)) {
-						var device = list[deviceId];
-						var structureId = device['structure_id'];
-						if (this.config["structureId"] && this.config["structureId"] !== structureId) {
-							this.log("Skipping device " + deviceId + " because it is not in the required structure. Has " + structureId + ", looking for " + this.config["structureId"] + ".");
-							continue;
-						}
-						var structure = data.structures[structureId];
-						var accessory = new DeviceType(this.conn, this.log, device, structure);
-						that.accessoryLookup[deviceId] = accessory;
-						foundAccessories.push(accessory);
-					}
-				}
-			}.bind(this);
+      const loadDevices = function(DeviceType) {
+        const list = data.devices && data.devices[DeviceType.deviceGroup];
+        for (const deviceId of Object.keys(list)) {
+          const device = list[deviceId];
+          const structureId = device.structure_id;
+          if (this.config.structureId && this.config.structureId !== structureId) {
+            this.log("Skipping device " + deviceId + " because it is not in the required structure. Has " + structureId + ", looking for " + this.config.structureId + ".");
+            continue;
+          }
+          const structure = data.structures[structureId];
+          const accessory = new DeviceType(this.conn, this.log, device, structure);
+          that.accessoryLookup[deviceId] = accessory;
+          foundAccessories.push(accessory);
+        }
+      }.bind(this);
 
-			loadDevices(ThermostatAccessory);
-			loadDevices(ProtectAccessory);
-			loadDevices(CamAccessory);
+      loadDevices(ThermostatAccessory);
+      loadDevices(ProtectAccessory);
+      loadDevices(CamAccessory);
 
-			return foundAccessories;
-		}.bind(this);
+      return foundAccessories;
+    }.bind(this);
 
-		var updateAccessories = function(data, accList) {
-			accList.map(function(acc) {
-				var device = data.devices[acc.deviceGroup][acc.deviceId];
-				var structureId = device['structure_id'];
-				var structure = data.structures[structureId];
-				acc.updateData(device, structure);
-			}.bind(this));
-		};
+    const updateAccessories = function(data, accList) {
+      accList.map(function(acc) {
+        const device = data.devices[acc.deviceGroup][acc.deviceId];
+        const structureId = device.structure_id;
+        const structure = data.structures[structureId];
+        acc.updateData(device, structure);
+      });
+    };
 
-		var handleUpdates = function(data){
-			updateAccessories(data, that.accessoryLookup);
-		};
-		setupConnection(this.config, this.log)
-			.then(function(conn){
-				that.conn = conn;
-				return that.conn.open();
-			})
-			.then(function(){
-				return that.conn.subscribe(handleUpdates);
-			})
-			.then(function(data) {
-				that.accessoryLookup = generateAccessories(data);
-				if (callback) {
-					var copy = that.accessoryLookup.map(function (a) { return a; });
-					callback(copy);
-				}
-			})
-			.catch(function(err) {
-				that.log.error(err);
-				if (callback) {
-					callback([]);
-				}
-			});
-	}
+    const handleUpdates = function(data){
+      updateAccessories(data, that.accessoryLookup);
+    };
+    setupConnection(this.config, this.log)
+      .then(function(conn){
+        that.conn = conn;
+        return that.conn.open();
+      })
+      .then(function(){
+        return that.conn.subscribe(handleUpdates);
+      })
+      .then(function(data) {
+        that.accessoryLookup = generateAccessories(data);
+        if (callback) {
+          const copy = Array.from(that.accessoryLookup);
+          callback(copy);
+        }
+      })
+      .catch(function(err) {
+        that.log.error(err);
+        if (callback) {
+          callback([]);
+        }
+      });
+  }
 };
