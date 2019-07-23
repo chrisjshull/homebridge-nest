e6c85b01b25943ae9fb2f1f3b31b48909933c823
Adrian Cable <adrian.cable@gmail.com>
2019-07-22 19:35:08 -0700

    More code clean-up.

--- index.js
+++ index.js
@@ -5,7 +5,6 @@ const Promise = require('bluebird');
 
 let Service, Characteristic, Accessory, uuid;
 let ThermostatAccessory, ProtectAccessory, CamAccessory; // DeviceAccessory,
-let Away, EcoMode, FanTimerActive, FanTimerDuration, HasLeaf, ManualTestActive, SunlightCorrectionEnabled, SunlightCorrectionActive, UsingEmergencyHeat;
 
 module.exports = function (homebridge) {
     Service = homebridge.hap.Service;
@@ -13,143 +12,11 @@ module.exports = function (homebridge) {
     Accessory = homebridge.hap.Accessory;
     uuid = homebridge.hap.uuid;
 
-    // Define custom characteristics
-
-    /*
-   * Characteristic "Away"
-   */
-    Away = function () {
-        Characteristic.call(this, 'Away', 'D6D47D29-4638-4F44-B53C-D84015DAEBDB');
-        this.setProps({
-            format: Characteristic.Formats.BOOL,
-            perms: [Characteristic.Perms.READ, Characteristic.Perms.WRITE, Characteristic.Perms.NOTIFY]
-        });
-        this.value = this.getDefaultValue();
-    };
-    inherits(Away, Characteristic);
-
-    /*
-   * Characteristic "EcoMode"
-   */
-    EcoMode = function () {
-        Characteristic.call(this, 'Eco Mode', 'D6D47D29-4639-4F44-B53C-D84015DAEBDB');
-        this.setProps({
-            format: Characteristic.Formats.BOOL,
-            perms: [Characteristic.Perms.READ, Characteristic.Perms.WRITE, Characteristic.Perms.NOTIFY]
-        });
-        this.value = this.getDefaultValue();
-    };
-    inherits(EcoMode, Characteristic);
-
-    /*
-   * Characteristic "FanTimerActive"
-   */
-    FanTimerActive = function () {
-        Characteristic.call(this, 'Fan Timer Active', 'D6D47D29-4640-4F44-B53C-D84015DAEBDB');
-        this.setProps({
-            format: Characteristic.Formats.BOOL,
-            perms: [Characteristic.Perms.READ, Characteristic.Perms.WRITE, Characteristic.Perms.NOTIFY]
-        });
-        this.value = this.getDefaultValue();
-    };
-    inherits(FanTimerActive, Characteristic);
-
-    /*
-   * Characteristic "FanTimerDuration"
-   */
-    FanTimerDuration = function () {
-        Characteristic.call(this, 'Fan Timer Duration', 'D6D47D29-4641-4F44-B53C-D84015DAEBDB');
-        this.setProps({
-            format: Characteristic.Formats.UINT8,
-            unit: Characteristic.Units.MINUTES,
-            maxValue: 60,
-            minValue: 15,
-            minStep: 15,
-            perms: [Characteristic.Perms.READ, Characteristic.Perms.WRITE, Characteristic.Perms.NOTIFY]
-        });
-        this.value = this.getDefaultValue();
-    };
-    inherits(FanTimerDuration, Characteristic);
-
-    /*
-   * Characteristic "HasLeaf"
-   */
-    HasLeaf = function () {
-        Characteristic.call(this, 'Has Leaf', 'D6D47D29-4642-4F44-B53C-D84015DAEBDB');
-        this.setProps({
-            format: Characteristic.Formats.BOOL,
-            perms: [Characteristic.Perms.READ, Characteristic.Perms.NOTIFY]
-        });
-        this.value = this.getDefaultValue();
-    };
-    inherits(HasLeaf, Characteristic);
-
-    /*
-   * Characteristic "ManualTestActive"
-   */
-    ManualTestActive = function () {
-        Characteristic.call(this, 'Manual Test Active', 'D6D47D29-4643-4F44-B53C-D84015DAEBDB');
-        this.setProps({
-            format: Characteristic.Formats.BOOL,
-            perms: [Characteristic.Perms.READ, Characteristic.Perms.NOTIFY]
-        });
-        this.value = this.getDefaultValue();
-    };
-    inherits(ManualTestActive, Characteristic);
-
-    /*
-   * Characteristic "SunlightCorrectionEnabled"
-   */
-    SunlightCorrectionEnabled = function () {
-        Characteristic.call(this, 'Sunlight Correction Enabled', 'D6D47D29-4644-4F44-B53C-D84015DAEBDB');
-        this.setProps({
-            format: Characteristic.Formats.BOOL,
-            perms: [Characteristic.Perms.READ, Characteristic.Perms.NOTIFY]
-        });
-        this.value = this.getDefaultValue();
-    };
-    inherits(SunlightCorrectionEnabled, Characteristic);
-
-    /*
-   * Characteristic "SunlightCorrectionActive"
-   */
-    SunlightCorrectionActive = function () {
-        Characteristic.call(this, 'Sunlight Correction Active', 'D6D47D29-4645-4F44-B53C-D84015DAEBDB');
-        this.setProps({
-            format: Characteristic.Formats.BOOL,
-            perms: [Characteristic.Perms.READ, Characteristic.Perms.NOTIFY]
-        });
-        this.value = this.getDefaultValue();
-    };
-    inherits(SunlightCorrectionActive, Characteristic);
-
-    /*
-   * Characteristic "UsingEmergencyHeat"
-   */
-    UsingEmergencyHeat = function () {
-        Characteristic.call(this, 'Using Emergency Heat', 'D6D47D29-4646-4F44-B53C-D84015DAEBDB');
-        this.setProps({
-            format: Characteristic.Formats.BOOL,
-            perms: [Characteristic.Perms.READ, Characteristic.Perms.NOTIFY]
-        });
-        this.value = this.getDefaultValue();
-    };
-    inherits(UsingEmergencyHeat, Characteristic);
-
     const exportedTypes = {
         Accessory: Accessory,
         Service: Service,
         Characteristic: Characteristic,
         uuid: uuid,
-        Away: Away,
-        EcoMode: EcoMode,
-        FanTimerActive: FanTimerActive,
-        FanTimerDuration: FanTimerDuration,
-        HasLeaf: HasLeaf,
-        ManualTestActive: ManualTestActive,
-        SunlightCorrectionEnabled: SunlightCorrectionEnabled,
-        SunlightCorrectionActive: SunlightCorrectionActive,
-        UsingEmergencyHeat: UsingEmergencyHeat
     };
 
     // DeviceAccessory =
