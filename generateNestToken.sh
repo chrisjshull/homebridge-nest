75305008e7c88090810b5ea43a4b6eed467b90b1
Adrian Cable <adrian.cable@gmail.com>
2019-08-24 07:57:40 -0700

    generateNestToken script rewritten so it doesn't require Python.

--- generateNestToken.sh
+++ generateNestToken.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+function jsonValue() {
+  KEY=$1
+  num=$2
+  awk -F"[,:}]" '{for(i=1;i<=NF;i++){if($i~/'$KEY'\042/){print $(i+1)}}}' | tr -d '"' | sed -n ${num}p
+}
+
 read -p "Email: " email
 read -p "Password: " password
 read -p "Do you have 2FA enabled? (y/n) " fa
@@ -10,20 +16,20 @@ token=$(curl -s -X "POST" "https://home.nest.com/session" \
 -H 'User-Agent: iPhone iPhone OS 11.0 Dropcam/5.14.0 com.nestlabs.jasper.release Darwin' \
 -H 'Content-Type: application/x-www-form-urlencoded; charset=utf-8' \
 --data-urlencode "email=$email" \
---data-urlencode "password=$password" | python3 -c "import sys, json; print(json.load(sys.stdin)['2fa_token'])")
+--data-urlencode "password=$password" | jsonValue 2fa_token 1)")
 read -p "2FA code received via SMS: " facode
 fatoken=$(curl -s -X "POST" "https://home.nest.com/api/0.1/2fa/verify_pin" \
 -H 'User-Agent: iPhone iPhone OS 11.0 Dropcam/5.14.0 com.nestlabs.jasper.release Darwin' \
 -H 'Content-Type: application/json; charset=utf-8' \
--d $'{"pin": "'"$facode"'","2fa_token": "'"$token"'"}' | python3 -c "import sys, json; print(json.load(sys.stdin)['access_token'])")
-echo "Your nest token is: "
+-d $'{"pin": "'"$facode"'","2fa_token": "'"$token"'"}' | jsonValue access_token 1)")
+echo "Your Nest access_token is: "
 echo $fatoken
 else
 token=$(curl -s -X "POST" "https://home.nest.com/session" \
 -H 'User-Agent: iPhone iPhone OS 11.0 Dropcam/5.14.0 com.nestlabs.jasper.release Darwin' \
 -H 'Content-Type: application/x-www-form-urlencoded; charset=utf-8' \
 --data-urlencode "email=$email" \
---data-urlencode "password=$password" | python3 -c "import sys, json; print(json.load(sys.stdin)['access_token'])")
-echo "Your nest token is: "
+--data-urlencode "password=$password" | jsonValue access_token 1)
+echo "Your Nest access_token is: "
 echo $token
 fi
