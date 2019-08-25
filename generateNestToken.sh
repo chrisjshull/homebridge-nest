6259e259be3c22b64322681d720fda359c83753d
Scott Hunter <coolcsh@gmail.com>
2019-08-24 19:19:06 -0700

    fixed extra quotes and parens

--- generateNestToken.sh
+++ generateNestToken.sh
@@ -16,12 +16,12 @@ token=$(curl -s -X "POST" "https://home.nest.com/session" \
 -H 'User-Agent: iPhone iPhone OS 11.0 Dropcam/5.14.0 com.nestlabs.jasper.release Darwin' \
 -H 'Content-Type: application/x-www-form-urlencoded; charset=utf-8' \
 --data-urlencode "email=$email" \
---data-urlencode "password=$password" | jsonValue 2fa_token 1)")
+--data-urlencode "password=$password" | jsonValue 2fa_token 1)
 read -p "2FA code received via SMS: " facode
 fatoken=$(curl -s -X "POST" "https://home.nest.com/api/0.1/2fa/verify_pin" \
 -H 'User-Agent: iPhone iPhone OS 11.0 Dropcam/5.14.0 com.nestlabs.jasper.release Darwin' \
 -H 'Content-Type: application/json; charset=utf-8' \
--d $'{"pin": "'"$facode"'","2fa_token": "'"$token"'"}' | jsonValue access_token 1)")
+-d $'{"pin": "'"$facode"'","2fa_token": "'"$token"'"}' | jsonValue access_token 1)
 echo "Your Nest access_token is: "
 echo $fatoken
 else
